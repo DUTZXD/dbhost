f366d32ebc45cf79050b4f833aa2ae4acbf277c3
zhangxiaodong91 <zhangxiaodong91@jd.com>
2021-08-09 18:01:02 +0800

    约饭bug成功修复

--- src/com/jd/db/MyGit.java
+++ src/com/jd/db/MyGit.java
@@ -3,7 +3,8 @@ package com.jd.db;
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("短视频");
-        System.out.println("约饭");
+        System.out.println("其它修复代码");
+        System.out.println("约饭 修复bug");
         System.out.println("斗地主");
         System.out.println("商城");
         System.out.println("打车");
